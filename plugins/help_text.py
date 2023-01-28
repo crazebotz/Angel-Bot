a58039c38f330c101adadc0ffc52f0be4f8ba5c5
anfargaming <120932993+anfargaming@users.noreply.github.com>
2023-01-28 14:43:48 +0530

    Create help_text.py

---
+++ plugins/help_text.py
@@ -0,0 +1,87 @@
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["help"]))
+async def help_user(bot, update):
+    if update.from_user.id in Config.AUTH_USERS:
+        # logger.info(update)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.HELP_USER,
+            parse_mode="html",
+            disable_web_page_preview=True,
+            reply_to_message_id=update.message_id
+        )
+
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
+async def start(bot, update):
+    if update.from_user.id in (Config.AUTH_USERS & Config.LAZY_DEVELOPER):
+        # logger.info(update)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.LAZY_DEVELOPER_TEXT.format(update.from_user.first_name),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("????????????????", url="https://t.me/BOTXZE_UPDATES"),
+                        InlineKeyboardButton("?? U????????e", url="https://t.me/BOTXZE"),
+                    ],
+                    [InlineKeyboardButton("?? ??????? O W N E R ??????? ??", url="https://t.me/anfar_mohammed")],
+                ]
+            ),
+            reply_to_message_id=update.message_id
+        )
+    elif update.from_user.id in Config.AUTH_USERS:
+        # logger.info(update)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.START_TEXT.format(update.from_user.first_name),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("????????????????", url="https://t.me/BOTXZE_UPDATES"),
+                        InlineKeyboardButton("?? U????????e", url="https://t.me/BOTXZE"),
+                    ],
+                    [InlineKeyboardButton("?? ??????? O W N E R ??????? ??", url="https://t.me/anfar_moahmmed")],
+                ]
+            ),
+            reply_to_message_id=update.message_id
+        )
+    else:
+        # logger.info(update) ==         
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.LAZY_START_TEXT.format(update.from_user.first_name),
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("?? ??????? O W N E R ??????? ??", url="https://t.me/anfar_moahmmed")],
+                    [
+                        InlineKeyboardButton("?¦??¦ UPDATE ¦??¦?", url="https://t.me/BOTXZE"),
+                    ],
+                    [InlineKeyboardButton("??? S U P P O R T ???", url="https://t.me/BOTXZE_UPDATES")],
+                ]
+            ),
+            reply_to_message_id=update.message_id
+        )
